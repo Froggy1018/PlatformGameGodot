9053b5d58edce770241f627bc5397a321cc49a4f
Froggy1018 <jihe2023@gmail.com>
2025-12-26 21:20:22 +0900

    Changed the sprite's speed to 400 and worked on the collision layers and created a science room scene

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300
+const SPEED = 400
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
